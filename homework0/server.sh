379c812a0bbb0cf5da5fd37573436e9a32fce3d3
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-11-09 16:08:40 +0100

    add port argument to server startup script

--- homework0/server.sh
+++ homework0/server.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-java -cp target/Hangman-0.0.1-SNAPSHOT.jar gr.kzps.id2212.hangman.server.TcpServer
+
+if [ "$#" -ge 1 ]; then
+    while getopts "p:" opt; do
+	case $opt in
+	    p)
+		java -cp target/Hangman-0.0.1-SNAPSHOT.jar gr.kzps.id2212.hangman.server.TcpServer --port $OPTARG
+		;;
+	    \?)
+		echo "Invalid option: -$OPTARG" >&2
+		;;
+	    :)
+		echo "Option -$OPTARG requires an argument." >&2
+		exit 1
+		;;
+	esac
+    done
+else
+    java -cp target/Hangman-0.0.1-SNAPSHOT.jar gr.kzps.id2212.hangman.server.TcpServer
+fi
