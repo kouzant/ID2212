597a53e8be2080f4e8465caa816af236cb9f29af
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-11-18 19:24:18 +0100

    Client can make a wish

---
+++ homework1/src/main/java/gr/kzps/id2212/marketplace/client/Commands/WishCommand.java
@@ -0,0 +1,22 @@
+package gr.kzps.id2212.marketplace.client.Commands;
+
+import gr.kzps.id2212.marketplace.client.Client;
+
+public class WishCommand extends BaseCommand {
+	private final String itemName;
+	private final float price;
+	
+	public WishCommand(Client client, String itemName, float price) {
+		super(client);
+		this.itemName = itemName;
+		this.price = price;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+}
