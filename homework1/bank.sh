55a6d777e2116c764224a9825c952c4b96162e7c
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-11-26 15:23:52 +0100

    One jar to rule them all. Init scripts

---
+++ homework1/bank.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp target/Marketplace-0.0.1-SNAPSHOT.jar se.kth.id2212.ex2.bankrmi.Server
---
+++ homework1/client.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp target/Marketplace-0.0.1-SNAPSHOT.jar gr.kzps.id2212.marketplace.client.ClientExecEnv
