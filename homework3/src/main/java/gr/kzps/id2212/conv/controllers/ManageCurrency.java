7301bc56ee0a552de4ad3bd208934b4f94a25845
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-12-03 14:20:41 +0100

    list already stored currencies

--- homework3/src/main/java/gr/kzps/id2212/conv/controllers/ManageCurrency.java
+++ homework3/src/main/java/gr/kzps/id2212/conv/controllers/ManageCurrency.java
@@ -1,10 +1,20 @@
 package gr.kzps.id2212.conv.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
+import javax.faces.validator.ValidatorException;
 
 import gr.kzps.id2212.conv.entities.Currency;
+import gr.kzps.id2212.conv.exceptions.ResultNotFound;
 import gr.kzps.id2212.conv.sessionbeans.CurrencyEJB;
 
 @ManagedBean
@@ -13,15 +23,35 @@ public class ManageCurrency {
 	@EJB
 	private CurrencyEJB currencyEJB;
 	private Currency currency;
+	private List<Currency> storedCurrencies;
 	
 	public ManageCurrency() {
 		currency = new Currency();
+	}
+	
+	@PostConstruct
+	public void init() {
+		try {
+			storedCurrencies = currencyEJB.findAllCurrencies();
+		} catch (ResultNotFound ex) {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "No currency", "No currencies stored so far");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		}
+		
 	}
 	
 	public String storeCurrency() {
 		currencyEJB.storeCurrency(currency);
 		
 		return null;
+	}
+
+	public List<Currency> getStoredCurrencies() {
+		return storedCurrencies;
+	}
+
+	public void setStoredCurrencies(List<Currency> storedCurrencies) {
+		this.storedCurrencies = storedCurrencies;
 	}
 
 	public Currency getCurrency() {
