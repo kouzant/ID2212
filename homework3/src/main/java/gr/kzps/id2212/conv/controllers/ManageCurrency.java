83e35e8b886b8f06055ad169d6defa6907e68ec4
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-12-03 16:27:14 +0100

    Basic conversion works

--- homework3/src/main/java/gr/kzps/id2212/conv/controllers/ManageCurrency.java
+++ homework3/src/main/java/gr/kzps/id2212/conv/controllers/ManageCurrency.java
@@ -1,17 +1,13 @@
 package gr.kzps.id2212.conv.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ComponentSystemEvent;
-import javax.faces.validator.ValidatorException;
 
 import gr.kzps.id2212.conv.entities.Currency;
 import gr.kzps.id2212.conv.exceptions.ResultNotFound;
