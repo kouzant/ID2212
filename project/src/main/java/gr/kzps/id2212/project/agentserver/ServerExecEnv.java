0b82b05b132c1fdb8b000b364943dcc2662ac088
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-12-11 20:01:28 +0100

    Add server IDs to log file

--- project/src/main/java/gr/kzps/id2212/project/agentserver/ServerExecEnv.java
+++ project/src/main/java/gr/kzps/id2212/project/agentserver/ServerExecEnv.java
@@ -10,6 +10,7 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.lookup.MainMapLookup;
 
 import gr.kzps.id2212.project.agentserver.overlay.Discovery;
 import gr.kzps.id2212.project.agentserver.overlay.PeerAgent;
@@ -33,6 +34,8 @@ public class ServerExecEnv {
 			CommandLine cmd = argsParser.parseArgs();
 			
 			serverId = cmd.getOptionValue("id");
+			
+			MainMapLookup.setMainArguments(new String[]{"id", serverId});
 			
 			if (cmd.hasOption("agentPort")) {
 				agentPort = Integer.parseInt(cmd.getOptionValue("agentPort"));
