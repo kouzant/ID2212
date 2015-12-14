783558ed6b1783ec9ce88c7e3c8fda49f30b5568
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-12-14 18:50:45 +0100

    Fix nodeA parameters

--- project/nodeA.sh
+++ project/nodeA.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-java -cp $1 gr.kzps.id2212.project.agentserver.ServerExecEnv -i server0 -s NodeA/
+java -cp $1 gr.kzps.id2212.project.agentserver.ServerExecEnv -i server0 -s NodeA/ \
+-ap 6060 -bp 9090
